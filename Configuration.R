9f0493d50ab6878c50db6d014bfc90d8853f4254
Michael Wynn <michael.wynn23@hotmail.com>
2023-01-10 16:17:16 +0800

    Test Commit

---
+++ Configuration.R
@@ -0,0 +1,5 @@
+# Import Libraries
+Packages <- c("tidyr", "ggplot2", "dplyr", "tibble", "quantmod", 
+              "TTR", "ggthemes", "corrplot" , "Hmisc", "datasets")
+
+lapply(Packages, library, character.only = TRUE)
